d0f64616f546d01786b038cf28d7fa41df7b81f1
Joanna Szymczak <jszymczak@xtm-intl.com>
2022-06-03 20:44:14 +0200

    Add thread and methods to start and stop the game

--- src/main/java/game/ducks/Game.java
+++ src/main/java/game/ducks/Game.java
@@ -1,14 +1,68 @@
 package game.ducks;
 
-public class Game
+public class Game implements Runnable
 {
     private GameView gameView;
     public int width;
     public int height;
+    public String title;
+    private boolean running = false;
+    private Thread thread;
+
     Game(String title, int width, int height)
     {
         this.width = width;
         this.height = height;
+        this.title = title;
+
+    }
+
+    private void init()
+    {
         gameView = new GameView(title, width, height);
+    }
+
+    private void update()
+    {
+
+    }
+
+    private void render()
+    {
+
+    }
+
+    public void run()
+    {
+        init();
+        while(running)
+        {
+            update();
+            render();
+        }
+
+        stop();
+    }
+
+    public synchronized void start()
+    {
+        if(running)
+            return;
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+    public synchronized void stop()
+    {
+        if(!running)
+            return;
+        running = false;
+        try
+        {
+            thread.join();
+        } catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
